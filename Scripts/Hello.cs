56326e8e04514803c50cce2140608a6d70dbf6d3
Takayuki Matsumura <op.66.allegro.agitato@gmail.com>
2022-02-23 15:47:34 +0900

    version up

--- Scripts/Hello.cs
+++ Scripts/Hello.cs
@@ -7,6 +7,6 @@ public class Hello : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello custom package");
+        Debug.Log("Hello custom package 0.1.1");
     }
 }
